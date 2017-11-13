7e0288da6141620de759b9be2463cb0545a9c861
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2017-11-13 15:53:22 +0200

    Removing Action Bar

--- app/src/main/java/com/example/android/spaceyquiz/MainActivity.java
+++ app/src/main/java/com/example/android/spaceyquiz/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.spaceyquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         // start the timer when application starts
         timeStart = System.currentTimeMillis();
