628ac7b9881b86a6aa5d74afe2d5c6b964f619ce
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2017-11-17 02:53:55 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/spaceyquiz/MainActivity.java
+++ app/src/main/java/com/example/android/spaceyquiz/MainActivity.java
@@ -16,29 +16,33 @@ public class MainActivity extends AppCompatActivity {
         // remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set activity_main as the xml layout for this activity
         setContentView(R.layout.activity_main);
-        // find views and create intents for activities
+        // Find each TextView and create implicit Intent
         TextView toSpaceQuiz = findViewById(R.id.to_space_quiz);
+        // On click: create the Intent and start the Intent
         toSpaceQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent toSpaceQuizIntent = new Intent(MainActivity.this, SpaceActivity.class);
+                Intent toSpaceQuizIntent = new Intent(MainActivity.this, SpaceActivity.class); // Intent for Space quiz
                 startActivity(toSpaceQuizIntent);
             }
         });
         TextView toOtherQuiz = findViewById(R.id.to_other_quiz);
+        // On click: create the Intent and start the Intent
         toOtherQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent toOtherQuizIntent = new Intent(MainActivity.this, GotActivity.class);
+                Intent toOtherQuizIntent = new Intent(MainActivity.this, GotActivity.class); // Intent for GameOfThrones quiz
                 startActivity(toOtherQuizIntent);
             }
         });
         TextView toOtherQuiz2 = findViewById(R.id.to_other_quiz2);
+        // On click: create the Intent and start the Intent
         toOtherQuiz2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent toOtherQuizIntent2 = new Intent(MainActivity.this, QuantumActivity.class);
+                Intent toOtherQuizIntent2 = new Intent(MainActivity.this, QuantumActivity.class); // Intent for Quantum Mechanics quiz
                 startActivity(toOtherQuizIntent2);
             }
         });
